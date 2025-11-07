0a34d11b43c0741f848db1aaa40442d1f71c00cf
Arda Hoke <hoke@berkeley.edu>
2025-11-06 22:06:14 -0800

    init

---
+++ pocket-garden/pocket-garden/pocket_gardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocket_gardenApp.swift
+//  pocket-garden
+//
+//  Created by Arda Hoke on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct pocket_gardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pocket-garden/pocket-gardenTests/pocket_gardenTests.swift
@@ -0,0 +1,17 @@
+//
+//  pocket_gardenTests.swift
+//  pocket-gardenTests
+//
+//  Created by Arda Hoke on 11/6/25.
+//
+
+import Testing
+@testable import pocket_garden
+
+struct pocket_gardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
